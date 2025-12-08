752d70614ae17fa8c2ccca2622931be5aba33f3b
Herminio García González <herminiogg@hotmail.com>
2025-12-08 14:51:29 +0100

    Added new perspective for persons.

---
+++ src/server/sparql/ehri/sparql_queries/SparqlQueriesPersons.js
@@ -0,0 +1,84 @@
+export const personsProperties = `
+  {
+    ?id rico:name ?prefLabel__id ;
+    BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+    BIND(?id as ?uri__id)
+    BIND(?id as ?uri__dataProviderUrl)
+    BIND(?id as ?uri__prefLabel)
+    BIND(CONCAT("/persons/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+    BIND(CONCAT("https://portal.ehri-project.eu/authorities/ehri_pers-", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?portalURI__prefLabel)
+    BIND(CONCAT("https://portal.ehri-project.eu/authorities/ehri_pers-", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?portalURI__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id rico:hasOrHadAgentName/rico:name ?otherLabels .
+  }
+  UNION
+  {
+    ?id rdfs:seeAlso ?seeAlso__id .
+    BIND(?seeAlso__id as ?seeAlso__dataProviderUrl)
+    BIND(?seeAlso__id as ?seeAlso__prefLabel)
+  }
+  UNION
+  {
+    ?id rico:date ?date .
+  }
+  UNION
+  {
+    ?id rico:hasBirthDate/rico:normalizedDateValue ?birthDate .
+  }
+  UNION
+  {
+    ?id rico:hasDeathDate/rico:normalizedDateValue ?deathDate .
+  }
+  UNION
+  {
+    ?id rico:generalDescription ?generalDescription .
+  }
+  UNION
+  {
+    ?id rico:history ?history .
+  }
+  UNION
+  {
+    ?id rico:performsOrPerformed [
+      rico:hasOrHadActivityType ehri:FunctionType ;
+      rico:generalDescription ?function 
+    ] .
+  }
+  UNION
+  {
+    ?id rico:performsOrPerformed [
+      rico:hasOrHadActivityType rico:OccupationType ;
+      rico:generalDescription ?occupation 
+    ] .
+  }
+  UNION
+  {
+    ?id rico:hasOrHadLegalStatus/rico:generalDescription ?legalStatus .
+  }
+  UNION
+  {
+    ?id rico:authorizedBy/rico:generalDescription ?mandate .
+  }
+  UNION
+  {
+    ?id ehri:structureOrGenealogy ?genealogy .
+  }
+  UNION
+  {
+    ?id ehri:sources ?sources .
+  }
+  UNION
+  {
+    SELECT ?id (COUNT(?description) AS ?numberOfConnectedDescriptions) WHERE {
+      ?id ^rico:hasOrHadSubject ?description .
+    } GROUP BY ?id
+  }
+`
+
+export const personsPlusLinkedDescriptionsProperties = personsProperties + `
+  BIND(CONCAT("/sampo/en/archivalDescriptions/faceted-search/table?page=0&constraints=%5B%7B%22facetClass%22%3A+%22archivalDescriptions%22%2C+%22facetId%22%3A%22ehriPersons%22%2C%22filterType%22%3A%22uriFilter%22%2C%22value%22%3A%7B%22node%22%3A%7B%22id%22%3A%22",  STR(?id), "%22%2C%22prefLabel%22%3A%22", STR(?prefLabel__id), "%22%7D%7D%7D%5D") AS ?archivalDescriptionsLink)
+  BIND(?archivalDescriptionsLink AS ?archivalDescriptionsLink__dataProviderUrl)
+  BIND("Use this person as filter in the archival descriptions perspective" AS ?archivalDescriptionsLink__prefLabel)
+`
